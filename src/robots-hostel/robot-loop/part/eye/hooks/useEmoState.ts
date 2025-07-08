d4f555e829281bcb3a1d1f5d8f7230fce82b10ef
Nxhzk <183066163@qq.com>
2025-07-08 17:40:42 +0800

    feat: 追踪防抖

--- src/robots-hostel/robot-loop/part/eye/hooks/useEmoState.ts
+++ src/robots-hostel/robot-loop/part/eye/hooks/useEmoState.ts
@@ -1,24 +1,38 @@
 import { useSelector } from 'react-redux'
 import { useEffect, useRef } from 'react';
 
-export const useEmoState = (setEmoState: any) => {
+export const useEmoState = (setEmoState: (state: string) => void) => {
     const { faceDetections } = useSelector((state: any) => state);
-    const timer = useRef<any>(null);
-    const dtimer = useRef<any>(null);
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
+    const delayTimerRef = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
-        if (faceDetections.length) {
-            clearTimeout(dtimer.current);
-            timer.current = setTimeout(() => {
-                (window as any).faceDetections = faceDetections;
-                setEmoState('gazeAt');
-            }, 3000);
+        if (faceDetections?.length) {
+            if (delayTimerRef.current) {
+                clearTimeout(delayTimerRef.current);
+                delayTimerRef.current = null;
+            }
+
+            if (!timerRef.current) {
+                timerRef.current = setTimeout(() => {
+                    (window as any).faceDetections = faceDetections;
+                    setEmoState('gazeAt');
+                    timerRef.current = null;
+                }, 1000);
+            }
         } else {
-            clearTimeout(timer.current);
-            dtimer.current = setTimeout(() => {
-                setEmoState('default');
-            }, 3000);
-         
+            if (timerRef.current) {
+                clearTimeout(timerRef.current);
+                timerRef.current = null;
+            }
+
+            if (!delayTimerRef.current) {
+                delayTimerRef.current = setTimeout(() => {
+                    setEmoState('default');
+                    delayTimerRef.current = null;
+                }, 1000);
+            }
         }
-    }, [faceDetections]);
+
+    }, [faceDetections, setEmoState]);
 }
